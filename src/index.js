bc82f983a346d794623577ada0e2155fc76918c6
AzharTakoy <azharahmedtakoy4444@gmail.com>
2023-03-24 17:47:05 +0300

    Complete assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+    const formito=document.querySelector('form')
+    formito.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const input=e.target.searchByID.value
+    console.log(input);
+
+  fetch(`http://localhost:3000/movies/${input}`)
+  .then(resp=>resp.json())
+  .then(data=>{
+   const title=document.querySelector('h4');
+   const summary=document.querySelector('p');
+    title.innerText=data.title;
+    summary.innerText=data.summary
+   })
+})
 }
+
 
 document.addEventListener('DOMContentLoaded', init);
